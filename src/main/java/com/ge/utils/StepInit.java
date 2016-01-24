310e588df5b708589161fb23b7b08d215aa6192a
Teodor Rupi <teodorrupi@gmail.com>
2016-01-24 13:44:44 +0100

    setting step initiator class as page objects collector and final user steps class

---
+++ src/main/java/com/ge/utils/StepInit.java
@@ -0,0 +1,12 @@
+package com.ge.utils;
+
+import com.ge.utils.pages.DictionaryPage;
+import net.thucydides.core.steps.ScenarioSteps;
+
+/**
+ * @author trupi
+ */
+public class StepInit extends ScenarioSteps {
+
+    protected DictionaryPage dictionaryPage;
+}
